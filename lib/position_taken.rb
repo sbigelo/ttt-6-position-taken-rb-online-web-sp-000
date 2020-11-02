080b4ac01e3bc041eca8a1dcaa347746c977486f
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-02 21:22:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  taken = nil
+  
   if (board[index] == "" || board[index] == " " || board[index] == nil)
     return false
   else board[index] == "X" || board[index] == "O"
